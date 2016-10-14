cd83d3dcb10dc2813fcb2603728cfccc188117a6
Patrick Bösch <patrick.boesch@viascom.ch>
2016-10-14 15:13:37 +0200

    Add JavaDoc

--- service-result/src/main/java/ch/viascom/groundwork/serviceresult/util/ObjectHasher.java
+++ service-result/src/main/java/ch/viascom/groundwork/serviceresult/util/ObjectHasher.java
@@ -6,8 +6,17 @@ import java.io.Serializable;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * Provides an object hashing function
+ */
 public class ObjectHasher {
 
+	/**
+	 * Create a hash of a serializable object
+     *
+	 * @param obj Serializable object
+	 * @return Hash of the serializable object
+	 */
 	public static String hash(Serializable obj) {
 
 		if (obj == null) {
@@ -15,7 +24,6 @@ public class ObjectHasher {
 		}
 		StringBuffer hexString = new StringBuffer();
 		try {
-			
 
 			MessageDigest m = MessageDigest.getInstance("SHA1");
 			m.update(SerializationUtils.serialize(obj));
