80816c8425b4c2af384a17fc2a8a98a8e5a1e86b
Patrick Bösch <patrick.boesch@viascom.ch>
2016-11-27 12:57:33 +0100

    Made SimpleRequests generic for other results

--- restclient-http/src/main/java/ch/viascom/groundwork/restclient/http/request/simple/SimplePutRequest.java
+++ restclient-http/src/main/java/ch/viascom/groundwork/restclient/http/request/simple/SimplePutRequest.java
@@ -2,49 +2,48 @@ package ch.viascom.groundwork.restclient.http.request.simple;
 
 import ch.viascom.groundwork.restclient.exception.RESTClientException;
 import ch.viascom.groundwork.restclient.http.request.PutRequest;
-import ch.viascom.groundwork.restclient.response.GenericResponse;
+import ch.viascom.groundwork.restclient.response.generic.Response;
+import lombok.Setter;
+import org.apache.http.Consts;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.HttpClient;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 /**
  * @author patrick.boesch@viascom.ch
  */
-public class SimplePutRequest extends PutRequest<GenericResponse> {
+public class SimplePutRequest<T extends Response> extends PutRequest<T> {
 
-    public SimplePutRequest(String url) throws RESTClientException {
-        super(url, HttpClientBuilder.create().build());
-        setPath("");
+    @Setter
+    private Class<T> parameterClass;
+
+    public SimplePutRequest(String url, Class<T> parameterClass) throws RESTClientException {
+        this(url, "application/json", new StringEntity("", Consts.UTF_8), HttpClientBuilder.create().build(), parameterClass);
     }
 
-    public SimplePutRequest(String url, String mediaType) throws RESTClientException {
-        super(url, HttpClientBuilder.create().build());
+    public SimplePutRequest(String url, HttpEntity requestBody, Class<T> parameterClass) throws RESTClientException {
+        this(url, "application/json", requestBody, HttpClientBuilder.create().build(), parameterClass);
+    }
+
+    public SimplePutRequest(String url, String mediaType, HttpEntity requestBody, Class<T> parameterClass) throws RESTClientException {
+        this(url, mediaType, requestBody, HttpClientBuilder.create().build(), parameterClass);
+    }
+
+    public SimplePutRequest(String url, String mediaType, HttpEntity requestBody, HttpClient httpClient, Class<T> parameterClass) throws RESTClientException {
+        this(url, "", mediaType, requestBody, httpClient, parameterClass);
+    }
+
+    public SimplePutRequest(String url, String path, String mediaType, HttpEntity requestBody, HttpClient httpClient, Class<T> parameterClass) throws RESTClientException {
+        super(url, httpClient);
+        setPath(path);
         setMediaType(mediaType);
-        setPath("");
+        setRequestBody(requestBody);
+        setParameterClass(parameterClass);
     }
 
-    public SimplePutRequest(String url, HttpEntity httpEntity) throws RESTClientException {
-        super(url, HttpClientBuilder.create().build());
-        setPath("");
-        setRequestBody(httpEntity);
-    }
-
-    public SimplePutRequest(HttpClient httpClient, String url) throws RESTClientException {
-        super(url, httpClient);
-        setUrl(url);
-        setPath("");
-    }
-
-    public SimplePutRequest(HttpClient httpClient, String url, String path) throws RESTClientException {
-        super(url, httpClient);
-        setUrl(url);
-        setPath(path);
-    }
-
-    public SimplePutRequest(HttpClient httpClient, String url, String path, HttpEntity httpEntity) throws RESTClientException {
-        super(url, httpClient);
-        setUrl(url);
-        setPath(path);
-        setRequestBody(httpEntity);
+    @Override
+    public Class<T> getParameterClass() {
+        return parameterClass;
     }
 }
