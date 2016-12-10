0463e821c5c2c2528af09feee2ec279fa4edad48
Patrick Bösch <patrick.boesch@viascom.ch>
2016-12-10 17:36:39 +0100

    Add interceptors for testing

---
+++ foxhttp/src/test/java/ch/viascom/groundwork/foxhttp/interceptors/RequestBodyInterceptor.java
@@ -0,0 +1,14 @@
+package ch.viascom.groundwork.foxhttp.interceptors;
+
+import ch.viascom.groundwork.foxhttp.interceptor.request.FoxHttpRequestBodyInterceptor;
+import ch.viascom.groundwork.foxhttp.interceptor.request.context.FoxHttpRequestBodyInterceptorContext;
+
+/**
+ * @author patrick.boesch@viascom.ch
+ */
+public class RequestBodyInterceptor implements FoxHttpRequestBodyInterceptor {
+    @Override
+    public void onIntercept(FoxHttpRequestBodyInterceptorContext context) {
+
+    }
+}
---
+++ foxhttp/src/test/java/ch/viascom/groundwork/foxhttp/interceptors/ResponseInterceptor.java
@@ -0,0 +1,14 @@
+package ch.viascom.groundwork.foxhttp.interceptors;
+
+import ch.viascom.groundwork.foxhttp.interceptor.response.FoxHttpResponseInterceptor;
+import ch.viascom.groundwork.foxhttp.interceptor.response.context.FoxHttpResponseInterceptorContext;
+
+/**
+ * @author patrick.boesch@viascom.ch
+ */
+public class ResponseInterceptor implements FoxHttpResponseInterceptor {
+    @Override
+    public void onIntercept(FoxHttpResponseInterceptorContext context) {
+
+    }
+}
