b0fd1622a8e305a60c90124ac93a3d1e580be3ac
Patrick Bösch <patrick.boesch@viascom.ch>
2016-12-21 15:21:15 +0100

    Fix javadoc

--- foxhttp/src/main/java/ch/viascom/groundwork/foxhttp/FoxHttpClient.java
+++ foxhttp/src/main/java/ch/viascom/groundwork/foxhttp/FoxHttpClient.java
@@ -95,7 +95,6 @@ public class FoxHttpClient {
      *
      * @param interceptorType    Type of the interceptor
      * @param foxHttpInterceptor Interceptor instance
-     * @return FoxHttpClientBuilder (this)
      * @throws FoxHttpException Throws an exception if the interceptor does not match the type
      */
     public void register(FoxHttpInterceptorType interceptorType, FoxHttpInterceptor foxHttpInterceptor) throws FoxHttpException {
