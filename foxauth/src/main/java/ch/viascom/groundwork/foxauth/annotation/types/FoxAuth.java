3782812427a75c16d264961faaa502cbdd0cd723
Patrick Bösch <patrick.boesch@viascom.ch>
2017-01-22 16:59:08 +0100

    Initinal commit FoxAuth

---
+++ foxauth/src/main/java/ch/viascom/groundwork/foxauth/annotation/types/FoxAuth.java
@@ -0,0 +1,12 @@
+package ch.viascom.groundwork.foxauth.annotation.types;
+
+import java.lang.annotation.*;
+
+/**
+ * @author patrick.boesch@viascom.ch
+ */
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FoxAuth {
+}
