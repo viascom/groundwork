42b2e94c16d9e27d457a6d304de1e40ec75566e6
Patrick Bösch <patrick.boesch@viascom.ch>
2017-01-22 17:06:11 +0100

    Name changes

---
+++ foxauth/src/main/java/ch/viascom/groundwork/foxauth/FoxAuth.java
@@ -0,0 +1,7 @@
+package ch.viascom.groundwork.foxauth;
+
+/**
+ * @author patrick.boesch@viascom.ch
+ */
+public class FoxAuth {
+}
--- foxauth/src/main/java/ch/viascom/groundwork/foxauth/FoxAuthReadInterceptor.java
+++ foxauth/src/main/java/ch/viascom/groundwork/foxauth/FoxAuthReadInterceptor.java
@@ -3,5 +3,5 @@ package ch.viascom.groundwork.foxauth;
 /**
  * @author patrick.boesch@viascom.ch
  */
-public class FoxAuthHandler {
+public interface FoxAuthReadInterceptor {
 }
