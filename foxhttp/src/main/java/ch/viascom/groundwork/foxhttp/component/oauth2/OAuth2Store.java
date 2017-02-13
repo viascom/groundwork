df6f33fb318d3c4dceea7a570f6363ba9d83ae51
Patrick Bösch <patrick.boesch@viascom.ch>
2017-02-13 23:07:49 +0100

    Add Javadoc

--- foxhttp/src/main/java/ch/viascom/groundwork/foxhttp/component/oauth2/OAuth2Store.java
+++ foxhttp/src/main/java/ch/viascom/groundwork/foxhttp/component/oauth2/OAuth2Store.java
@@ -15,30 +15,84 @@ import java.util.Map;
 public class OAuth2Store {
 
     //Request
+    /**
+     * Grant type used for token requests
+     */
     private GrantType grantType;
+    /**
+     * Token request endpoint
+     */
     private String authUrl;
+    /**
+     * Token request method
+     */
     private RequestType authRequestType = RequestType.POST;
+    /**
+     * Requested scopes
+     */
     private String requestScopes = "";
+    /**
+     * AuthorizationScope which needs a token
+     */
     private FoxHttpAuthorizationScope authScope;
+    /**
+     * Additional parameters which will be included in the token request
+     */
     private Map<String, String> additionalParameters = new HashMap<>();
+    /**
+     * Should Client Credentials be used for all token requests?
+     */
     private boolean useClientCredentials = false;
 
     //Password
+    /**
+     * Username for password grant type
+     */
     private String username;
+    /**
+     * Password for password grant type
+     */
     private String password;
 
     //ClientCredentials
+    /**
+     * Client id for Client Credentials grant type or if client credentials are used for requests (useClientCredentials)
+     */
     private String clientId;
+    /**
+     * Client secret for Client Credentials grant type or if client credentials are used for requests (useClientCredentials)
+     */
     private String clientSecret;
 
     //AuthorizationCode
+    /**
+     * Authorization Code for AuthorizationCode grant type
+     */
     private String authorizationCode;
 
     //Response
+    /**
+     * Refresh token provided by the server
+     */
     private String refreshToken;
+    /**
+     * Access token provided by the server
+     */
     private String accessToken = "";
+    /**
+     * Expiration time in seconds provided by the server
+     */
     private Long expirationTimeSeconds;
+    /**
+     * Create date time of the access token
+     */
     private DateTime accessTokenTime;
+    /**
+     * Calculated expiration date time based on accessTokenTime and expirationTimeSeconds
+     */
     private DateTime accessTokenExpirationTime;
+    /**
+     * Granted scopes by the server
+     */
     private String scopes;
 }
