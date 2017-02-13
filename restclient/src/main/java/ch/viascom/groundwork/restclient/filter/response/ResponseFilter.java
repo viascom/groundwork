f411a54e53211e13cbb408561ccd3cb3859b4788
Patrick Bösch <patrick.boesch@viascom.ch>
2017-02-13 19:49:43 +0100

    Reorganize restclient components

--- restclient/src/main/java/ch/viascom/groundwork/restclient/filter/response/ResponseFilter.java
+++
@@ -1,9 +0,0 @@
-package ch.viascom.groundwork.restclient.filter.response;
-
-import ch.viascom.groundwork.restclient.filter.RESTFilter;
-
-/**
- * @author patrick.boesch@viascom.ch
- */
-public interface ResponseFilter extends RESTFilter {
-}
