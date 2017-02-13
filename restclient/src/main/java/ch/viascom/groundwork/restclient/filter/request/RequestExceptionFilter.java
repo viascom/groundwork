1c3f9ec58cb57d30800983cc2c2cae44a3ba8e73
Patrick Bösch <patrick.boesch@viascom.ch>
2017-02-13 19:49:43 +0100

    Reorganize restclient components

--- restclient/src/main/java/ch/viascom/groundwork/restclient/filter/request/RequestExceptionFilter.java
+++
@@ -1,9 +0,0 @@
-package ch.viascom.groundwork.restclient.filter.request;
-
-import ch.viascom.groundwork.restclient.filter.RESTFilter;
-
-/**
- * @author patrick.boesch@viascom.ch
- */
-public interface RequestExceptionFilter extends RESTFilter {
-}
